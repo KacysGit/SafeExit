c225cb3a01a30f61094bc3a7a87a54084b002e75
Kacy <yot5@txstate.edu>
2023-11-12 23:49:17 -0600

    added CustomizeCall button and Header

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react"
 import {Text, View, StyleSheet} from "react-native";
+import CustomizeCall from "./components/CustomizeCall"
+import Header from "./components/header"
 
 export default function App(){
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}> Hello World </Text>
-      <Text style={styles.textStyle}> Lets Stop Sexual Harassment </Text>
-
+      <Header />
+      <CustomizeCall />
     </View>
+
+    
   )
 }
 
@@ -15,9 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black", // Set your desired color here
-  },
-  textStyle: {
-    color: "white", // Change this to your desired text color
-    fontSize: 20,
+    display: "flex"
   },
 });
---
+++ components/header.js
@@ -0,0 +1,37 @@
+// This file adds the banner at the top of the screen
+// * App name
+// * settings icon
+
+import React, { useState, useRef } from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    Button,
+    TouchableOpacity
+} from "react-native";
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headerText}>SafelyExit</Text>
+        </View>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    header: {
+      backgroundColor: "grey", // Background color of the header
+      padding: 20, // Adjust padding as needed
+      alignItems: "left", // Center align the text
+      flexDirection: "row"
+    },
+    headerText: {
+      color: "white", // Color of the header text
+      fontSize: 24, // Font size of the header text
+      fontWeight: "bold", // Bold font weight for header text
+    },
+})
