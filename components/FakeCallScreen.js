1d58e9631ffad5aea9cb1ab0c1075d576827e2b1
Kacy <yot5@txstate.edu>
2023-12-05 00:08:32 -0600

    rough draft of call page

---
+++ components/FakeCallScreen.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // If you're using Expo or ensure you have the equivalent in your project
+
+const FakeCallScreen = ({ onHangUp, onAccept }) => {
+
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <View style={styles.fullScreen}>
+      <View style={styles.topContainer}>
+        <Text style={styles.time}>{currentTime}</Text>
+        <Text style={styles.callerName}>Unknown</Text>
+      </View>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity onPress={onHangUp} style={[styles.button, styles.declineButton]}>
+          <Ionicons name="md-close" size={50} color="#fff" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onAccept} style={[styles.button, styles.acceptButton]}>
+          <Ionicons name="md-call" size={50} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  fullScreen: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#283933', // Adjust the color to match your example
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    zIndex: 1000,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'green', // This should match your example image's background color
+  },
+  topContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  time: {
+    color: 'white',
+    fontSize: 36,
+    fontWeight: '600',
+  },
+  callerName: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: '400',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingBottom: 40, // Adjust padding as needed
+    width: '100%',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    width: 75, // Adjust the size to match your example
+    height: 75, // Adjust the size to match your example
+    borderRadius: 37.5, // This should be half the width/height to be perfectly round
+    padding: 15,
+  },
+  declineButton: {
+    backgroundColor: '#FF3B30', // Adjust the color to match your example
+  },
+  acceptButton: {
+    backgroundColor: '#30D158', // Adjust the color to match your example
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    marginTop: 5,
+  },
+
+});
+
+export default FakeCallScreen;
