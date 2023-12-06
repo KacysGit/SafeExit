502a843d9508d7b2b4f5d7e0608bb21bacb5c9c5
Kacy <yot5@txstate.edu>
2023-12-06 02:33:41 -0600

    buttons have labels but off center

---
+++ Docs/Goals.md
@@ -0,0 +1,3 @@
+This file contains all the features I want to add
+
+The user should be able to set a code on the keypad to indicate to their network if they are okay or a code to indicate if they are in danger
--- Views/AnswerCallScreen.js
+++ Views/AnswerCallScreen.js
@@ -36,26 +36,14 @@ const AnswerCallScreen = ({ callerName, onHangUp }) => {
         {/* Buttons */}
         <View style={styles.buttonsCenterContainer}>
           <View style={styles.buttonsContainer}>
-            <TouchableOpacity style={styles.button}>
-              <Ionicons name="mic-off" size={40} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Ionicons name="keypad" size={40} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Ionicons name="volume-high" size={40} color="white" />
-            </TouchableOpacity>
+            <ButtonWithLabel iconName="mic-off" label="mute" />
+            <ButtonWithLabel iconName="keypad" label="keypad" />
+            <ButtonWithLabel iconName="volume-high" label="audio" />
           </View>
-          <View style={styles.buttonsContainer}>
-            <TouchableOpacity style={styles.button}>
-             <Ionicons name="add-outline" size={45} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Ionicons name="videocam" size={40} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-             <Ionicons name="person-circle-outline" size={40} color="white" /> 
-            </TouchableOpacity>
+          <View style={[styles.buttonsContainer, styles.secondButtonsContainer]}>
+            <ButtonWithLabel iconName="add-outline" label="add call" />
+            <ButtonWithLabel iconName="videocam" label="FaceTime" />
+            <ButtonWithLabel iconName="person-circle-outline" label="contacts" />
           </View>
         </View>
 
@@ -66,6 +54,14 @@ const AnswerCallScreen = ({ callerName, onHangUp }) => {
     </ImageBackground>
   );
 };
+
+// Define a separate component for buttons with labels
+const ButtonWithLabel = ({ iconName, label }) => (
+  <TouchableOpacity style={styles.button}>
+    <Ionicons name={iconName} size={40} color="white" />
+    <Text style={styles.label}>{label}</Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
   fullScreen: {
@@ -105,23 +101,36 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   buttonsCenterContainer: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1.5, // Adjust this to allow for more space for the buttons
+    justifyContent: 'center', // This centers the buttons vertically
     alignItems: 'center',
-    marginBottom: 250,
+    paddingHorizontal: 20, // Add padding to maintain spacing from screen edges
+    marginBottom: 160
   },
   buttonsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
+    width: '100%', // Take the full width of the container to space out the buttons
   },
   button: {
+    marginHorizontal: 10, // Keep the buttons spaced out horizontally
+    marginBottom: 20, // Add some space below the first row of buttons
     width: 70,
     height: 70,
-    backgroundColor: '#44424c', // Adjust button background color
-    borderRadius: 35, // Half the width/height to make it round
+    backgroundColor: '#44424c',
+    borderRadius: 35,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  secondButtonsContainer: {
+    marginTop: 30, // Add space above the second row of buttons to push it down from the first row
+  },
+  label: {
+    color: 'white',
+    fontSize: 12, // Adjust the size as needed
+    marginTop: 4, // Space between icon and label
+    textAlign: 'center',
   },
 });
 
